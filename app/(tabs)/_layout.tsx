e7f6c3c9eff9263efbdf79552608eb9fac2fa94f
Itokawa <t.itokawa1999@gmail.com>
2024-12-08 18:17:16 +0900

    いろいろ追加

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 import { useState } from 'react';
-import { Pressable } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 
 export default function TabLayout() {
   const [showTabBar, setShowTabBar] = useState(true);
@@ -11,6 +11,8 @@ export default function TabLayout() {
   };
 
   return (
+    // タブとかについての参照先
+    // https://docs.expo.dev/router/advanced/tabs/
     <Tabs screenOptions={{
       tabBarActiveTintColor: 'blue',
       tabBarStyle: {
@@ -22,15 +24,17 @@ export default function TabLayout() {
         options={{
           title: 'ホーム',
           headerRight: () => (
-            <Pressable 
-              onPress={toggleTabBar}
-              style={{ marginRight: 15 }}
-            >
-              <FontAwesome 
-                name={showTabBar ? 'eye-slash' : 'eye'} 
-                size={24} 
-              />
-            </Pressable>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 15 }}>
+              <Text style={{ marginRight: 8 }}>
+                {showTabBar ? 'タブを非表示' : 'タブを表示'}
+              </Text>
+              <Pressable onPress={toggleTabBar}>
+                <FontAwesome 
+                  name={showTabBar ? 'eye-slash' : 'eye'} 
+                  size={24} 
+                />
+              </Pressable>
+            </View>
           ),
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
         }}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,12 @@ import { Stack } from 'expo-router/stack';
 
 export default function Layout() {
   return (
+    // スタックとかについての参照先
+    // https://docs.expo.dev/router/advanced/stack/
     <Stack>
       {/* (tabs)ディレクトリのデフォルトのヘッダーを非表示 */}
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* (tabs2)ディレクトリのデフォルトのヘッダーを非表示 */}
       <Stack.Screen name="(tabs2)" options={{ headerShown: false }} />
     </Stack>
   );
